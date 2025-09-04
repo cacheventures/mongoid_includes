29bc82ee224e29e4f988d1948b04beb1d0699b87
Jarrett Lusso <jclusso@gmail.com>
2025-09-04 15:32:21 -0400

    Bump version

--- lib/mongoid/includes/version.rb
+++ lib/mongoid/includes/version.rb
@@ -5,6 +5,6 @@ module Mongoid
   module Includes
 
     # Public: This library will attempt to follow semantic versioning (whatever that's supposed to be).
-    VERSION = '3.0.2'
+    VERSION = '3.0.3'
   end
 end

186a12bd103d0cd67f55a72c083714d98a60a931
Daniel Arnold <darnold8@gmail.com>
2025-10-07 10:13:51 -0400

    release 3.0.4: update version file and Gemfiles. also remove version file comments.

--- lib/mongoid/includes/version.rb
+++ lib/mongoid/includes/version.rb
@@ -1,10 +1,5 @@
-# Mongoid is an ODM (Object Document Mapper) Framework for MongoDB, written in Ruby.
 module Mongoid
-  # Improves eager loading in Mongoid, supporting polymorphic associations,
-  # and up to two-levels of eager loading.
   module Includes
-
-    # Public: This library will attempt to follow semantic versioning (whatever that's supposed to be).
-    VERSION = '3.0.3'
+    VERSION = '3.0.4'
   end
 end
